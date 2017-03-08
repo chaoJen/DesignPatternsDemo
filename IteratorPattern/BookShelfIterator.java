34a9b3569791660de2108326eab929de85d0887c
ChaoJen <chao.jen.main@gmail.com>
2017-03-08 12:42:27 +0800

    add Iterator Pattern

---
+++ IteratorPattern/BookShelfIterator.java
@@ -0,0 +1,22 @@
+public class BookShelfIterator implements Iterator {
+
+	private BookShelf bookShelf;
+	private int index;
+
+	public BookShelfIterator(BookShelf bookShelf) {
+		
+		this.bookShelf = bookShelf;
+	}
+
+	public boolean hasNext() {
+		
+		return index < bookShelf.getLength();
+	}
+
+	public Object next() {
+
+		Book book = bookShelf.getBookAt(index);
+		++index;
+		return book;
+	}
+}
---
+++ IteratorPattern/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+	
+	public static void main(String[] args) {
+
+		BookShelf bookShelf = new BookShelf(4);
+		bookShelf.appendBook(new Book("Around the World in 80 Days"));
+		bookShelf.appendBook(new Book("Bible"));
+		bookShelf.appendBook(new Book("Cinderella"));
+		bookShelf.appendBook(new Book("Daddy-Long_legs"));
+
+		Iterator iterator = bookShelf.iterator();
+
+		while (iterator.hasNext()) {
+			Book book = (Book) iterator.next();
+			System.out.println(book.getName());
+		}
+	}
+}
