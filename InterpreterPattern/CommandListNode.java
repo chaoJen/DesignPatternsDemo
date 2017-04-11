5e454f854b613b7410710d56380471ecf083e2c5
ChaoJen <chao.jen.main@gmail.com>
2017-04-11 11:07:49 +0800

    add InterpreterPattern

---
+++ InterpreterPattern/CommandListNode.java
@@ -0,0 +1,34 @@
+import java.util.Vector;
+
+// <command list> ::= <command>* end
+public class CommandListNode extends Node {
+
+	private Vector list = new Vector();
+
+	public void parse(Context context) throws ParseException {
+
+		while (true) {
+
+			if (context.currentToken() == null) {
+				
+				throw new ParseException("Mission 'end'");
+			
+			} else if (context.currentToken().equals("end")) {
+				
+				context.skipToken("end");
+				break;
+			
+			} else {
+
+				Node commandNode = new CommandNode();
+				commandNode.parse(context);
+				list.add(commandNode);
+			}
+		}
+	}
+
+	public String toString() {
+
+		return "" + list;
+	}
+}
